9bc44f3a4df1dabe66d0b10af2a334d5d106a3d5
govindnh4cl <govindsinghrathore@outlook.com>
2016-12-25 20:10:24 -0500

    Corrected convert_seqs.py with comments

--- scripts/convert_seqs.py
+++ scripts/convert_seqs.py
@@ -1,27 +1,65 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import os.path  
+import fnmatch  
+import shutil  
 
-import os
-import glob
-import cv2 as cv
+# --------- Configurable parameters start ----------- #
+# Does not write files on disk. Just prints their names on console.
+# Used for generating a file containing the names of images relative to the
+# base path(without extension, but includes prefix e.g. 'set01/V000/' )
+print_names = 0
 
+# Make interval 1 if want to print all file names
+# Used for generating 1x, 10x training/test set for caltech
+if print_names:
+    interval = 10  
 
-def save_img(dname, fn, i, frame):
-    cv.imwrite('{}/{}_{}_{}.png'.format(
-        out_dir, os.path.basename(dname),
-        os.path.basename(fn).split('.')[0], i), frame)
+# A list of all sets to be parsed
+sets = ['set00', 'set01', 'set02', 'set03', 'set04', 
+'set05', 'set06', 'set07', 'set08','set09', 'set10']
 
-out_dir = 'data/images'
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-for dname in sorted(glob.glob('data/set*')):
-    for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
-        cap = cv.VideoCapture(fn)
-        i = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            save_img(dname, fn, i, frame)
-            i += 1
-        print(fn)
+#sourcedir must have directories set00, set01 etc
+sourcedir = '/media/disk2/govind/work/dataset/caltech/data'
+#outdir will have directories set00/V000/*jpg, set00/V001/ .. set01/V000/ etc 
+outdir = '/media/disk2/govind/work/dataset/caltech/data/JPEGImages'
+# --------- Configurable parameters end ----------- #
+   
+def open_save(seq_file, outdir, set, seq_name):  
+    # read .seq file
+    with open(seq_file,'rb') as f:
+        string = str(f.read())  
+    # split .seq file into segment with the image prefix  
+    splitstring = '\xFF\xD8\xFF\xE0\x00\x10\x4A\x46\x49\x46'
+    strlist=string.split(splitstring)  
+    
+    # create directories
+    set_out_dir = os.path.join(outdir, set);
+    seq_out_dir = os.path.join(outdir, set, seq_name.split('.')[0]);
+    if not os.path.exists(set_out_dir): os.mkdir(set_out_dir)    
+    if not os.path.exists(seq_out_dir): os.mkdir(seq_out_dir)    
+    
+    # deal with file segment, every segment is an image except the first one 
+    # Skip the first one, which is filled with .seq heade    
+    for idx, img in enumerate(strlist[1:]):
+        filename = str(idx + 1) + '.jpg' 
+        filenamewithpath=os.path.join(outdir, set, seq_name.split('.')[0], filename)         
+        if print_names:
+            if (idx % interval) == 0:
+                print os.path.join(set, seq_name.split('.')[0], filename)
+        else:
+            with open(filenamewithpath,'wb+') as i:
+                i.write(splitstring + img)  
+   
+if __name__=="__main__":
+    if not os.path.exists(outdir): os.mkdir(outdir)
+    
+    for set in sorted(sets):
+        print 'Parsing ', set,
+        setdir = os.path.join(sourcedir, set);
+        if not os.path.exists(setdir):
+            print 'Cannot find diectory: %s. Skipping.' % (setdir)
+            continue
+            
+        for seq_name in sorted(os.listdir(setdir)):
+            if seq_name.endswith(".seq"): 
+                open_save(os.path.join(setdir, seq_name), outdir, set, seq_name)
+        print 'Done'
