6e642ccb4db1d1b3121bea783ae8b3109dcfe333
govindnh4cl <govindsinghrathore@outlook.com>
2016-12-28 11:49:38 -0500

    Removed .jpg extensions while printing names

--- scripts/convert_seqs.py
+++ scripts/convert_seqs.py
@@ -6,19 +6,22 @@ import shutil
 # Does not write files on disk. Just prints their names on console.
 # Used for generating a file containing the names of images relative to the
 # base path(without extension, but includes prefix e.g. 'set01/V000/' )
-print_names = 0
+print_names = 1
 
 # Make interval 1 if want to print all file names
 # Used for generating 1x, 10x training/test set for caltech
 if print_names:
-    interval = 10  
+    interval = 30
 
 # A list of all sets to be parsed
 sets = ['set00', 'set01', 'set02', 'set03', 'set04', 
 'set05', 'set06', 'set07', 'set08','set09', 'set10']
 
+#sets = ['set00', 'set01', 'set02', 'set03', 'set04', 'set05'] #train
+#sets = ['set06', 'set07', 'set08','set09', 'set10'] #test
+
 #sourcedir must have directories set00, set01 etc
-sourcedir = '/media/disk2/govind/work/dataset/caltech/data'
+sourcedir = '/media/disk2/govind/work/dataset/caltech/unzip'
 #outdir will have directories set00/V000/*jpg, set00/V001/ .. set01/V000/ etc 
 outdir = '/media/disk2/govind/work/dataset/caltech/data/JPEGImages'
 # --------- Configurable parameters end ----------- #
@@ -41,10 +44,10 @@ def open_save(seq_file, outdir, set, seq_name):
     # Skip the first one, which is filled with .seq heade    
     for idx, img in enumerate(strlist[1:]):
         filename = str(idx + 1) + '.jpg' 
-        filenamewithpath=os.path.join(outdir, set, seq_name.split('.')[0], filename)         
+        filenamewithpath=os.path.join(outdir, set, seq_name, filename)         
         if print_names:
             if (idx % interval) == 0:
-                print os.path.join(set, seq_name.split('.')[0], filename)
+                print os.path.join(set, seq_name, filename.split('.')[0])
         else:
             with open(filenamewithpath,'wb+') as i:
                 i.write(splitstring + img)  
@@ -53,7 +56,7 @@ if __name__=="__main__":
     if not os.path.exists(outdir): os.mkdir(outdir)
     
     for set in sorted(sets):
-        print 'Parsing ', set,
+        #print 'Parsing ', set,
         setdir = os.path.join(sourcedir, set);
         if not os.path.exists(setdir):
             print 'Cannot find diectory: %s. Skipping.' % (setdir)
@@ -62,4 +65,4 @@ if __name__=="__main__":
         for seq_name in sorted(os.listdir(setdir)):
             if seq_name.endswith(".seq"): 
                 open_save(os.path.join(setdir, seq_name), outdir, set, seq_name)
-        print 'Done'
+        #print 'Done'
